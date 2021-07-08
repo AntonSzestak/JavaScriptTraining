7e9de286cd8b1b735353f9e92efe3afe81c3dae2
AntonSzestak <87122242+AntonSzestak@users.noreply.github.com>
2021-07-08 15:56:03 +0300

    Add files via upload

---
+++ untitled5/notGardeningApp.js
@@ -0,0 +1,7 @@
+const name = prompt('Введи имя, пока не поздно!');
+let age = prompt('А теперь мне нужно узнать твой возраст')
+let num = Number(age);
+const ageCof = 29;
+let ageFin = num + ageCof;
+alert(name + ' Тебе в 2050 году будет ' + ageFin + ' лет! ')
+
